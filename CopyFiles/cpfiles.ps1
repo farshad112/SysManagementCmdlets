fdcb671bab445101efa4eed05ad286714115a4b8
Farshad Bin Fazle <farshad1563@gmail.com>
2018-01-07 22:22:19 +0600

    Added initial cpfiles script
    Script is not finished. Working on help section.

---
+++ CopyFiles/cpfiles.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.Synopsys
+Copies files and folder recursively from specified directory to destination directory. 
+Supports copying on both local and remote filesystem.
+.Description
+This script copies subdirectories and files under a specified directory to the destination directory.
+User has to specify the source and destination directory as command line argument. It is useful for
+recursive copy and paste task on remote server management.   
+.Parameter srcDir
+This is the source directory path
+.Parameter destDir
+This is the destination directory path
+.Parameter remoteHostName
+Names of remote servers
+.Example 
+cpfiles -srcDir C:\SampleFiles -destDir D:\
+.Example
+cpfiles -srcDir C:\SampleFiles -destDir D:\ -remoteHostName user-pc
+.Example
+cpfiles -srcDir C:\SampleFiles -destDir D:\ -remoteHostName user1-pc,user2-pc
+#>
+
--- CopyFiles/readme.md
+++ CopyFiles/readme.md
@@ -1,6 +1,6 @@
 # CopyFiles
 
 ## Description
-A simple powershell script that will copy files and folders from one location to the next.
+A simple powershell script that will recursively copy files and folders from one location to the next.
 
 ## Example 1
